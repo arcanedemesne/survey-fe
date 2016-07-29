33e40bc0ad835959864f13f644f8c46a721adf8d
Jennifer Allen <jallen@deloitte.com>
2016-07-29 14:53:03 -0400

    package.json change

--- build/app/survey/survey.action.types.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var SURVEY_SAVE = exports.SURVEY_SAVE = 'SURVEY_SAVE_CHANGES';
-var STORE_SURVEY_TEMPLATE = exports.STORE_SURVEY_TEMPLATE = 'STORE_SURVEY_TEMPLATE';
-var INITIALIZE = exports.INITIALIZE = 'INITIALIZE';
